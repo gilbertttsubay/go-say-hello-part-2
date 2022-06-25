44215b8d29062b4670dfdd2acbc79dfbbca257ec
Gilbert Subay <gilbertttsubay@gmail.com>
2022-06-25 17:14:02 +0700

    add function in modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gilbertttsubay/go-say-hello-part-2
+
+go 1.17
---
+++ say_hello.go
@@ -0,0 +1,7 @@
+package bagian_2_go_modules
+
+import "fmt"
+
+func SayHello(name string) {
+	fmt.Println("Hello", name)
+}
